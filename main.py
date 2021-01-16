30d879280da3adae28b3d17f8931498ce1aba163
tabaraemi <emanuel.tabara@info.uaic.ro>
2021-01-16 20:02:57 +0200

    initial comiit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+emo_cats = ["Bored", "Sad", "Happy", "Enthusiast"]
+bored_arr = ["plictisesc", "naspa", "urat", "dorm"]
+sad = ["trist","trists" ,"plang","lacrima","lacrimi", "sinucid", "omor", "tem","frica"]
+happy_arr = ["rad","zambesc","fericit","fericita","usurata","bucuros","bucuorasa"]
+enthusiasm_arr = ["interesant","fain","place"]
+
+def tokenize():
+    words = []
+    return words
+
+
+def makegraphs(data):
+    graphs = []
+    print(graphs)
+    return  graphs
+
+
+def splitByCategory(text):
+    categories = []
+    return categories
+
+
+def main():
+    print("in main")
+
+
